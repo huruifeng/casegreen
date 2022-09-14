e060d0c6d3dda4899d7fc6ffd068055d26822f78
RuifengHU <442490020@qq,con>
2022-09-13 20:43:07 -0400

    updates

--- casegreen/settings.py
+++ casegreen/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ###############
-LOGOUT_REDIRECT_URL = "login"
+LOGIN_URL = "login"
 LOGOUT_REDIRECT_URL = "login"
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
